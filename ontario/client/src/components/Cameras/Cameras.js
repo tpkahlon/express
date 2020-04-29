6a7e4f6eb61a5490746f294955832794d41f8848
Tej <tejpartapsingh91@gmail.com>
2020-04-29 19:47:49 -0400

    revise

--- ontario/client/src/components/Cameras/Cameras.js
+++ ontario/client/src/components/Cameras/Cameras.js
@@ -4,26 +4,47 @@ import { Form, Row, Col } from "react-bootstrap";
 import { uniqBy } from "lodash";
 
 const Cameras = ({ cameras }) => {
-  const [data, setData] = useState({ allCameras: [], currentCity: null });
+  const revisedData = cameras
+    .filter((i) => i.Status === "Enabled")
+    .sort((a, b) => (a.Name.toLowerCase() < b.Name.toLowerCase() ? -1 : 1));
+  const [data, setData] = useState({
+    data: revisedData,
+    currentCity: null,
+    currentRoadway: null,
+  });
+  const filterData =
+    data.currentRoadway === null &&
+    data.currentRoadway === "" &&
+    data.currentCity === null &&
+    data.currentCity === ""
+      ? revisedData
+      : data.currentCity && data.currentRoadway
+      ? revisedData.filter(
+          (i) =>
+            i.CityName === data.currentCity &&
+            i.RoadwayName === data.currentRoadway
+        )
+      : data.currentCity
+      ? revisedData.filter((i) => i.CityName === data.currentCity)
+      : data.currentRoadway
+      ? revisedData.filter((i) => i.RoadwayName === data.currentRoadway)
+      : revisedData;
   const handleChange = (e) => {
     const { name, value } = e.target;
     setData({ ...data, [name]: value });
   };
-  const revisedCameras = cameras
-    .filter((i) => i.Status === "Enabled")
-    .sort((a, b) => (a.Name.toLowerCase() < b.Name.toLowerCase() ? -1 : 1));
-  const filterCamerasByCity =
-    data.currentCity === null || data.currentCity === ""
-      ? revisedCameras
-      : revisedCameras.filter((i) => i.CityName === data.currentCity);
-  const cities = uniqBy(revisedCameras, (e) => e.CityName)
+  const cities = uniqBy(revisedData, (e) => e.CityName)
     .map((i) => i.CityName)
+    .filter((el) => el != null && el !== "")
+    .sort((a, b) => (a.toLowerCase() < b.toLowerCase() ? -1 : 1));
+  const roadways = uniqBy(revisedData, (e) => e.RoadwayName)
+    .map((i) => i.RoadwayName)
     .filter((el) => el != null && el !== "")
     .sort((a, b) => (a.toLowerCase() < b.toLowerCase() ? -1 : 1));
   return (
     <>
-      <Form>
-        <Form.Group controlId="city">
+      <Form as={Row}>
+        <Form.Group as={Col} xs={12} sm={6} controlId="city">
           <Form.Label>Filter Cameras by City</Form.Label>
           <Form.Control
             as="select"
@@ -39,9 +60,25 @@ const Cameras = ({ cameras }) => {
             ))}
           </Form.Control>
         </Form.Group>
+        <Form.Group as={Col} xs={12} sm={6} controlId="city">
+          <Form.Label>Filter Cameras by Roadway</Form.Label>
+          <Form.Control
+            as="select"
+            custom
+            name="currentRoadway"
+            onChange={handleChange}
+          >
+            <option value="">Select a Roadway</option>
+            {roadways.map((i, index) => (
+              <option value={i} key={index}>
+                {i}
+              </option>
+            ))}
+          </Form.Control>
+        </Form.Group>
       </Form>
       <Row>
-        {filterCamerasByCity.map((camera) => (
+        {filterData.map((camera) => (
           <Col xs sm md={6} className="mb-3" key={camera.Id}>
             <Camera camera={camera} />
           </Col>
